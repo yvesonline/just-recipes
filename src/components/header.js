08b8d7cf97e416ca1735999ae291d1fd8273b7c7
Yves Weissig <yves@yvesonline.de>
2020-04-13 17:34:48 +0100

    Moving files around and adding navigation

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,10 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default () => (
-  <div>Header</div>
+  <div>
+    Header
+    <Link to="/">Index</Link>
+    <Link to="/recipes/">Recipes</Link>
+  </div>
 )
--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from "../components/layout"
 
 export default ({ data }) => {
   console.log(data)
   return (
-    <div>
+    <Layout>
       <h1>My Site's Recipes</h1>
       <table>
         <thead>
@@ -26,7 +27,7 @@ export default ({ data }) => {
           ))}
         </tbody>
       </table>
-    </div>
+    </Layout>
   )
 }
 
