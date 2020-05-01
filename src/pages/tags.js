afb85909271d8cbfa0e2dd1cd2388cfffca5fcd7
Yves Weissig <yves@weissig.me>
2020-05-01 14:29:26 +0100

    Sorting tags by frequency

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -4,7 +4,11 @@ import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 
 export default ({ data }) => {
+  const threshold = 10
   let keywords = new Map()
+  let keywordsReduced = new Map()
+  let keywordsSorted = undefined
+  // Compile map of keywords with frequencies
   data.allRecipe.edges.forEach(({ node }) => {
     node.fields.keywords.forEach((item) => {
       if (keywords.has(item)) {
@@ -14,11 +18,19 @@ export default ({ data }) => {
       }
     })
   })
+  // Filter by threshold
+  keywords.forEach((value, key) => {
+    if (value >= threshold) {
+      keywordsReduced.set(key, value)
+    }
+  })
+  // Sort by frequencies
+  keywordsSorted = new Map([...keywordsReduced.entries()].sort((a, b) => b[1] - a[1]));
   return (
     <Layout>
       <SEO title="Tags" description="Our overview of tags" />
       <div className="field is-grouped is-grouped-multiline">
-        {Array.from(keywords.entries(), ([key, value]) => (
+        {Array.from(keywordsSorted.entries(), ([key, value]) => (
           <div className="control" key={key}>
             <div className="tags has-addons">
               <span className="tag is-light is-capitalized"><Link className="has-text-black" to={"/tags/" + key}>{key.replace(/-/g, " ")}</Link></span>
